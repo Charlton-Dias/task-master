2bc928a7f93c0997cc87236f0597bbb69d8e570a
Joseito Fernandes <joseito.terence@gmail.com>
2023-04-25 23:03:21 +0530

    show logout button when not logged in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,29 @@
 import { Button } from '@mui/material'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Login = () => {
+  const session = useSession()
+
+  if (session.status === 'authenticated') {
+    return (
+      <>
+        <Button
+          sx={{ mt: 3, mb: 2 }}
+          variant='outlined'
+          color='success'
+          fullWidth
+          onClick={() => {
+            signOut()
+              .then(() => console.log('success'))
+              .catch(err => console.log(err))
+          }}
+        >
+          Logout
+        </Button>
+      </>
+    )
+  }
+
   return (
     <>
       <Button
@@ -11,12 +33,8 @@ const Login = () => {
         fullWidth
         onClick={() => {
           signIn()
-            .then(() => {
-              console.log('success')
-            })
-            .catch(err => {
-              console.log(err)
-            })
+            .then(() => console.log('success'))
+            .catch(err => console.log(err))
         }}
       >
         Login
