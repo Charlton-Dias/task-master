3097f8eefbc4417e6b05c888c50bf4fc1991aa87
Joseito Fernandes <joseito.terence@gmail.com>
2023-04-26 20:34:08 +0530

    basig design for login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 const Login = () => {
   const session = useSession()
-  const router = useRouter()
 
   if (session.status === 'authenticated') {
     return (
@@ -15,9 +14,7 @@ const Login = () => {
           color='success'
           fullWidth
           onClick={() => {
-            signOut()
-              .then(() => console.log('success'))
-              .catch(err => console.log(err))
+            void signOut()
           }}
         >
           Logout
@@ -27,21 +24,72 @@ const Login = () => {
   }
 
   return (
-    <>
-      <Button
-        sx={{ mt: 3, mb: 2 }}
-        variant='outlined'
-        color='success'
-        fullWidth
-        onClick={() => {
-          signIn()
-            .then(() => router.push('/'))
-            .catch(err => console.log(err))
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: '650px',
+          height: '300px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 'auto',
+          letterSpacing: '0.3rem',
+          textTransform: 'uppercase',
+          borderRadius: '.5rem',
+          boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+          overflow: 'hidden'
         }}
       >
-        Login
-      </Button>
-    </>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '100%',
+            overflow: 'hidden',
+            maxWidth: '420px'
+          }}
+          bgcolor='grey.900'
+        >
+          <Typography variant='h1' fontWeight='700' fontSize='5rem'
+            color='white'
+            p={2}
+          >
+            Task Master
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '68%',
+            overflow: 'hidden',
+          }}
+        >
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={() => {
+              void signIn("github", { callbackUrl: '/' })
+            }}
+          >
+            <GitHubIcon sx={{ mr: 1 }} />
+            Login with GitHub
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
